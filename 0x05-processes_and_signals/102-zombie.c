11257c156a6c8d29f80cc0ff1dfc49c51c4d5443
A-bdallah <abdallah179ahmed@gmail.com>
2024-05-03 16:22:39 -0700

    fix task adv 3

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Creates an infinite loop
+ *
+ * Return: Always returns 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
 
 /**
  * main - Entry point of the program.
@@ -17,28 +30,19 @@ int main(void)
 	for (i = 0; i < 5; i++)
 	{
 		child_pid = fork();
-		if (child_pid == -1)
+		if (child_pid < 0)
 		{
-			perror("fork");
+			perror("Fork failed");
 			exit(EXIT_FAILURE);
 		}
 		else if (child_pid == 0)
 		{
+			/*Child process*/
 			printf("Zombie process created, PID: %d\n", getpid());
 			exit(EXIT_SUCCESS);
 		}
 	}
-
-	/* Parent process waits for all child processes to complete */
-	for (i = 0; i < 5; i++)
-	{
-		if (wait(NULL) == -1)
-		{
-			perror("wait");
-			exit(EXIT_FAILURE);
-		}
-	}
-
+	/*infinite loop */
+	infinite_while();
 	return (0);
 }
-
