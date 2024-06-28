a0f0a9bd75aa18149df3209c9f14952d36c06499
A-bdallah <abdallah179ahmed@gmail.com>
2024-06-28 15:33:49 -0700

    tsk numb 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,15 @@
-# install flask 2.1.0 from pip3
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3',
+# File: install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
 package { 'Werkzeug':
-    ensure => '2.1.1',
-    provider => 'pip3',
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
